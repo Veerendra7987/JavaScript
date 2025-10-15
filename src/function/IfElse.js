7e5d762de3ec3b662e198fa11fca5c54970e67f9
Veerendra7987 <ashuahirwar512@gmail.com>
2025-10-15 11:54:42 +0530

    Create IfElse.js

---
+++ src/function/IfElse.js
@@ -0,0 +1,14 @@
+// let hour=14;
+
+// if(hour<12){
+//   console.log("Good Morning");
+// }else{
+//   console.log("Good AfterNoon");
+// }
+
+let LoggedIn = false;
+ if (LoggedIn) {
+   console.log("User is logged in.");
+ } else {
+   console.log("Please log in to continue.");
+ }
