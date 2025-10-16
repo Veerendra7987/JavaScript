65b5d6aeba1d44f41c4f30dae75032652bc867dc
Veerendra7987 <ashuahirwar512@gmail.com>
2025-10-16 16:41:17 +0530

    Create ElseIf.js

---
+++ src/function/ElseIf.js
@@ -0,0 +1,16 @@
+let num=75;
+
+if(num>=90){
+  console.log("You Have Earned Grade : A");
+}else if (num>=70)
+{
+  console.log("You Have Earned Grade : B");
+}else if (num>=50)
+{
+  console.log("You Have Earned Grade : C");
+}else if (num>=30)
+{
+  console.log("You Have Earned Grade : D");
+}else{
+  console.log("Fail ho gaye");
+}
